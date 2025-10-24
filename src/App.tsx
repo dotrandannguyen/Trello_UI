11ce4606e0b47a6e62d1bbe80589d68b0b1520ef
dotrandannguyen <nguyendoo0101@gmail.com>
2025-10-24 16:36:17 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,72 @@
-import {useState} from 'react'
-  import { Button } from "@/components/ui/button"
-export function App(){
-  const [count, setCount] = useState(0)
-  function handleClick(){
-    setCount(count +1)
-  
-}
+import { useState } from 'react';
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import ixios from 'axios';
 
-return (
-  <div className = 'w-full h-[200px] b-[150px] bg-red-500'>
-  <button onClick = {handleClick} >
-      <Button>Click me</Button>
-    push:{count}
-  </button></div>
-)
+
+export function App(){
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+
+  function handleChangeName(event: React.ChangeEvent<HTMLInputElement>){
+    setName(event.target.value);
+  }
+
+   async function handleSubmit(e){
+    e.preventDefault();
+    console.log(name, email, password);
+    try{
+      const response = await ixios.post('http://localhost:3000/auth/register', {
+        name,
+        email,
+        password
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
+  function handleClick(){
+
+  }
+
+
+
+    return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className="w-[400px]">
+        <h1 className="text-2xl font-bold text-center">Create Account</h1>
+        <p>Please fill out the form below to create an account.</p>
+
+        <form onSubmit={handleSubmit}>
+          <div className="mt-4">
+            <label htmlFor="name">Name</label>
+            <Input placeholder="Name" type="text" id="name" value={name} onChange={handleChangeName} />
+          </div>
+          <div className="mt-4">
+            <label htmlFor="email">Email</label>
+            <Input placeholder="Email" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="password">Password</label>
+            <Input placeholder="Password" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <Input placeholder="Confirm Password" type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+          </div>
+
+          <Button className="mt-4" type="submit">Create Account</Button>
+        </form>
+      </div>
+    </div>
+  )
 }
